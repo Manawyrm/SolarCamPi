49140ca182415561cf958c746d0145bbea337b55
Tobias Mädel <t.maedel@alfeld.de>
2019-07-25 22:19:15 +0200

    Updated readSetting tool

--- pidata/readSettings.sh
+++ pidata/readSettings.sh
@@ -3,17 +3,25 @@
 echo "Voltage: "
 echo $(((`i2cget -y 1 0x08 1 c` << 8) + `i2cget -y 1 0x08 0 c`))
 
-echo "Sleep-Interval (seconds):"
+echo "Sleep-Interval (seconds) - high supply voltage:"
 echo $(((`i2cget -y 1 0x08 3 c` << 8) + `i2cget -y 1 0x08 2 c`))
 
-echo "Undervoltage Lockout (mV):"
+echo "Sleep-Interval (seconds) - low supply voltage:"
 echo $(((`i2cget -y 1 0x08 5 c` << 8) + `i2cget -y 1 0x08 4 c`))
 
-echo "Undervoltage Hysteresis (mV):"
+echo "Low supply voltage threshold (mV):"
 echo $(((`i2cget -y 1 0x08 7 c` << 8) + `i2cget -y 1 0x08 6 c`))
 
+echo "Undervoltage Lockout (mV):"
+echo $(((`i2cget -y 1 0x08 9 c` << 8) + `i2cget -y 1 0x08 8 c`))
+
+echo "Undervoltage Hysteresis (mV):"
+echo $(((`i2cget -y 1 0x08 11 c` << 8) + `i2cget -y 1 0x08 10 c`))
+
 echo "Disable Timeout flag: "
-echo `i2cget -y 1 0x08 8 c`
+echo `i2cget -y 1 0x08 12 c`
 
 echo "Timeout: "
-echo $(((`i2cget -y 1 0x08 10 c` << 8) + `i2cget -y 1 0x08 9 c`))
+echo $(((`i2cget -y 1 0x08 14 c` << 8) + `i2cget -y 1 0x08 13 c`))
+
+
